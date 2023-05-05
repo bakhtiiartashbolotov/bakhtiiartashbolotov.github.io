76b3cf47f3ec74343270726066cfbad8171a1fa7
bakhtiiartashbolotov <bakhtiiar.tashbolotov@alatoo.edu.kg>
2023-05-05 22:37:29 +0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const techArray = ['laptops', 'headphones', 'phones', 'watches', 'laptop', 'notebook', 'headphone', 'phone', 'watch', 'clock', 'computer', 'pc', 'computers'];
 const fashionArray = ['fashion','mens clothing', 'womens clothing', 'clothing', 'clothes', 'pants', 'shirt', 'leggins', 'polo', 'cardigan'];
 const autoArray = ['oil', 'car', 'auto', 'wheel', 'oil', 'wheels', 'for auto', 'for car', 'all-terrain', 'castrol', 'valvoline', 'shell rotella']
+const jewelryArray = ['jewelry','necklaces', 'necklace', 'ring', 'rings', 'gold', 'silver', 'diamond', 'golden']
+const foodArray = ['food', 'meat', 'fruit', 'fruits', 'seafood', 'fresh', 'vegetables', 'beef', 'salmon', 'chicken', 'banana', 'strawberries', 'strawberry', 'bananas', 'apple', 'apples']
 
 form = document.querySelector('form');
 form.addEventListener('submit', event => {
@@ -14,6 +16,10 @@ form.addEventListener('submit', event => {
         url = 'fashion.html';
     } else if (autoArray.includes(query)) {
         url = 'auto.html';
+    } else if (jewelryArray.includes(query)){
+        url = 'jewelry.html'
+    } else if (foodArray.includes(query)){
+        url = 'food.html'
     } else {
         url = `${query}.html`;
     }
